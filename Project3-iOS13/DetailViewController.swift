a28feaca87983f5a4121e8f4058e479b44281cb2
Dennis <nndenninn@gmail.com>
2020-05-08 15:37:01 +0300

    Test app for ActivityViewController

---
+++ Project3-iOS13/DetailViewController.swift
@@ -0,0 +1,52 @@
+//
+//  DetailViewController.swift
+//  Project3-iOS13
+//
+//  Created by Dennis Nesanoff on 08.05.2020.
+//  Copyright © 2020 Dennis Nesanoff. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    var selectedImage: String?
+    var numberOfPicture: Int?
+    var countOfPictures: Int?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.largeTitleDisplayMode = .never
+        
+        if let selectedImage = selectedImage,
+            let number = numberOfPicture,
+            let count = countOfPictures {
+            imageView.image = UIImage(named: selectedImage)
+            title = "Picture \(number) of \(count)"
+        }
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        navigationController?.hidesBarsOnTap = false
+    }
+    
+    @objc func shareTapped() {
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+            print("No image found")
+            return
+        }
+        
+        let activityVC = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        activityVC.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(activityVC, animated: true)
+    }
+}
--- Project3-iOS13/ViewController.swift
+++ Project3-iOS13/ViewController.swift
@@ -8,13 +8,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    var pictures = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let fm = FileManager()
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+        
+        pictures.sort()
+        
+        title = "Storm Viewer"
+        navigationController?.navigationBar.prefersLargeTitles = true
     }
-
-
 }
 
+// MARK: - UITableViewDataSource
+extension ViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        cell.accessoryType = .disclosureIndicator
+        
+        return cell
+    }
+}
+
+// MARK: - UITableViewDelegate
+extension ViewController {
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let dvc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController else { return }
+        dvc.selectedImage = pictures[indexPath.row]
+        dvc.numberOfPicture = indexPath.row + 1
+        dvc.countOfPictures = pictures.count
+//        present(dvc, animated: true)
+        navigationController?.pushViewController(dvc, animated: true)
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
